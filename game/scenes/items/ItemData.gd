dc557cb7a1ab7b391c49a65a5d9656b390123aac
Mammy <zamotokhina.masha@mail.ru>
2025-12-16 20:51:57 +0400

    распределение

--- game/scenes/items/ItemData.gd
+++ game/scenes/items/ItemData.gd
@@ -3,3 +3,12 @@ class_name ItemData
 
 @export var name: String
 @export var icon: Texture2D
+@export var description: String
+
+@export var speed: int
+@export var power: int
+@export var health: int
+
+@export var key: int
+
+@export var artefact: int
